4265fdc3e7f93ba0f8f57b7fe5a0d23e1d4a2371
樋口 翔 <higuchisyo@LAB-N1695.local>
2019-09-20 11:56:15 +0900

    add: gitignore file , search function

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -22,42 +22,27 @@ class search extends Component {
   onSubmit(e) {
     e.preventDefault();
     console.log(this.state.req);
-    // fetch("http://localhost:8080/api/v1/tags/search", {
-    //   method: "POST",
-    //   headers: {
-    //     Accept: "application/json",
-    //     "Content-Type": "application/json"
-    //   },
-    //   body: JSON.stringify(this.state.req)
-    // })
-    //   .then(response => response.json())
-    //   .then(responseJson => {
-    //     this.setState({
-    //       res: JSON.stringify({ responseJson: "a" })
-    //     });
-    //   })
-    //   .catch(error => {
-    //     console.error(error);
-    //   });
+
     var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        if (this.response) {
-          console.log(this.response);
-          this.setState({
-            res: JSON.stringify(this.response)
-          });
-        }
-      } else if (this.readyState == 4) {
-        alert("失敗");
+    var responseJson;
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        // this.setState({ res: xhr.response });
+        var responseJson = JSON.parse(xhr.response);
+        console.log(responseJson[0]);
+
+        this.setState({
+          res: responseJson
+        });
       }
     };
+
     var jsonText = JSON.stringify(this.state.req); // ここで、dataをJSON文字列に変換
     xhr.open("POST", "http://localhost:8080/api/v1/tags/search");
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.send(jsonText);
 
-    console.log(this.state.res);
+    console.log(this.state);
   }
 
   onTextAreaChange(e) {
@@ -79,6 +64,7 @@ class search extends Component {
                         {options}
                     </select>
                 </div> */}
+
         <div className="form-group mx-sm-3 mb-2">
           <input
             className="form-control mr-sm-2"
@@ -95,7 +81,13 @@ class search extends Component {
             検索
           </button>
         </div>
-        {this.state.res}
+
+        <ol>
+          {this.state.res.map(function(index) {
+            return <li key={index.id}>{index.tagName}</li>;
+          })}
+        </ol>
+        {/* {JSON.stringify(this.state.res)} */}
       </form>
     );
   }
